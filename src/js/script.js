ca5cb3ef608c6ef2970ea81105e4b239aca60f61
k kalo <khaledkalo17@gmail.com>
2020-07-17 12:00:48 +0200

    the secound Update

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
 
     $('[data-toggle=tooltip]').tooltip();
 
+    $('[data-add-to-cart]').click(function(e){
+        alert('Zu Warenkorb Hinzufügen');
+        e.stopPropagation();
+    });
+
 });
